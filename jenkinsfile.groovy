e0531c846a02459380e26397472104cf997ae43c
adminsm <stefan.mindermann@gmail.com>
2020-02-26 20:45:04 +0100

    AZLXDOC13 depl.

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,6 +8,9 @@ pipeline {
           environment{
                 def DOCKER_HUB_USERNAME = credentials('docker-hub-username')
                 def DOCKER_HUB_PASSWORD = credentials('docker-hub-password')
+                def AZLXDOC13_user = credentials('AZLXDOC13_user')
+                def AZLXDOC13_pw = credentials('AZLXDOC13_pw')
+            
                 def ENVIRONMENT_CODE= ''
             }
     stages{
@@ -53,15 +56,15 @@ pipeline {
         }
 
 
-        /*stage("Remote Deploy"){
-            //TODO: do branch check- If Development branch ---> Deploy to development environment, If master branch --> Deploy to Production Environment
+        stage("Remote Deploy"){
+            //TODO: deploy to AZLXDOC13
             steps{
                 script{
                     def remote = [:]
-                    remote.name = 'Chords Central Web host'
-                    remote.host = 'chords-central-web-1.speelyaal.io'
-                    remote.user = 'root'
-                    remote.password = 'chords@december'
+                    remote.name = 'AZLXDOC13'
+                    remote.host = '23.101.76.43'
+                    remote.user = AZLXDOC13_user
+                    remote.password = AZLXDOC13_pw
                     remote.allowAnyHosts = true
                       //Stop all containers
                       //sshCommand remote: remote, command: "docker container stop speelyaal/atlassian-app-visualize-issues"
@@ -69,21 +72,21 @@ pipeline {
 
 
                         //Remove all containers
-                      sshCommand remote: remote, command: "docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=speelyaal/chords-central-web:tst --format=\"{{.ID}}\"))", failOnError: false
+                      sshCommand remote: remote, command: "docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=smindermann/docker-rep-pub-01:v1.0 --format=\"{{.ID}}\"))", failOnError: false
 
                         //Delete the existing images
-                      sshCommand remote: remote, command: "docker rmi speelyaal/chords-central-web:tst", failOnError: false
+                      sshCommand remote: remote, command: "docker rmi smindermann/docker-rep-pub-01:v1.0", failOnError: false
 
                         //Docker Login
-                       sshCommand remote: remote, command: "docker login -u speelyaal -p ${DOCKER_HUB_PASSWORD}"
+                       sshCommand remote: remote, command: "docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}"
                         //Start the app again
-                      sshCommand remote: remote, command: "docker run -p 1205:80 -d speelyaal/chords-central-web:tst"
+                      sshCommand remote: remote, command: "docker run -p 5100:4200 -d smindermann/docker-rep-pub-01:v1.0"
 
 
 
                 }
             }
-        }*/
+        }
 
         stage("Cleanup Jenkins"){
                 steps{
